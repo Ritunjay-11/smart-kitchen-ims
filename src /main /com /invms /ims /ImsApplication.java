f90a2818fac08bbcfdf0001615c4182c270d3493
Ritunjay-11 <66157748+Ritunjay-11@users.noreply.github.com>
2024-04-07 14:19:56 +0530

    Create ImsApplication.java

---
+++ src /main /com /invms /ims /ImsApplication.java
@@ -0,0 +1,13 @@
+package com.invms.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsApplication.class, args);
+	}
+
+}
