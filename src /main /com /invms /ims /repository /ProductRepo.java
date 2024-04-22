e571823062aa686cea616e9a692492d7fa97e5a3
rishabhta2812 <150886455+rishabhta2812@users.noreply.github.com>
2024-04-23 02:01:47 +0530

    ProductRepo.java

--- src /main /com /invms /ims /repository /ProductRepo.java
+++ src /main /com /invms /ims /repository /ProductRepo.java
@@ -1,11 +1,22 @@
 package com.invms.ims.repo;
 
-import org.springframework.data.mongodb.repository.MongoRepository; 
-import org.springframework.stereotype.Repository; 
-  
-import com.invms.ims.model.Product; 
-  
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.invms.ims.model.Product;
+
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface ProductRepo extends MongoRepository<Product, String>{ 
-  
-} 
+public interface ProductRepo extends MongoRepository<Product, String> {
+
+    // MongoDB uses different syntax for queries, we use the `@Query` annotation here
+    @Query("{'minimumProducts' : {$lt : ?0}}")
+    List<Product> findByMinimumProductsLessThan(int threshold);
+
+    @Query("{'expirationDate' : {$lte : ?0}}")
+    List<Product> findByExpirationDateLessThanEqual(LocalDate date);
+
+}
